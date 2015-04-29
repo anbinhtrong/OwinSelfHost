1bdb03168f4db3acca629eaa49c10ff9d5f626e6
Án Bình Trọng <minhtam.seta@gmail.com>
2015-04-30 05:05:29 +0700

    Get all methods from web api

--- SelfHost/Program.cs
+++ SelfHost/Program.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
 
 namespace SelfHost
 {
@@ -10,6 +15,33 @@ namespace SelfHost
     {
         static void Main(string[] args)
         {
+            string baseAddress = "http://localhost:9000/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(baseAddress))
+            {
+                // Create HttpCient and make a request to api/values 
+                HttpClient client = new HttpClient();
+
+                var response = client.GetAsync(baseAddress + "api/values").Result;
+                
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                var config = new HttpConfiguration();
+                IApiExplorer apiExplorer = Startup.MyConfiguration.Services.GetApiExplorer();
+                foreach (var apiDescription in apiExplorer.ApiDescriptions)
+                {
+                    Console.WriteLine("Uri path: {0}", apiDescription.RelativePath);
+                    Console.WriteLine("HTTP method: {0}", apiDescription.HttpMethod);
+                    foreach (ApiParameterDescription parameter in apiDescription.ParameterDescriptions)
+                    {
+                        Console.WriteLine("Parameter: {0} - {1}", parameter.Name, parameter.Source);
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            Console.ReadLine(); 
         }
     }
 }
